ee93d2f1acd443ef7b3017ed4ee6838f19a11234
Aiemu <479983336@qq.com>
2019-09-01 00:26:26 +0800

    hw3 re finished, TODO: state code handle

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import re
 
 def analyse():
@@ -8,21 +9,26 @@ def analyse():
     
     input_path = cmd_list[0]
     output_path = cmd_list[1]
+    url_list = []
 
     try:
         with open(input_path, "r") as fi:
-           pass
+            url_list = fi.readlines()
+            for row in url_list:
+                dict = {}
+                ret = requests.get(row)
+                ret.encoding = 'utf-8'
+                html = ret.text
+                dict["phone"] = re.findall(r"(?<=[^\w])(\d{7,8}|\d{11})(?=[^\w])", html)
+                dict["name"] = re.findall(r"[(姓名)(name)][:：](.*?)[,.;\"，。；“”]", html)
+                dict["url"] = re.findall(r"\s*(?<=[\s,.;，。；])网址[:：]\s*(https?://.*?)(?=[,;\"，。；“”\s])", html)
+                dict["shared"] = re.findall(r"[^\n\r]*链接[^\n\r]*提取码[^\n\r]*", html)
+                js = json.dumps(dict)
+                with open(output_path, "w", encoding = "utf-8") as fo:
+                    fo.write(js)
 
     except IOError:
         print("Error: Input file not exist!")
-        return
-
-    try:
-        with open(output_path, "w") as fo:
-            pass
-
-    except IOError:
-        print("Error: Output file creat failed!")
         return
 
 analyse()

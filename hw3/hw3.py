193a21190fdedf998b76007ba1f6e09403ae362c
Aiemu <479983336@qq.com>
2019-09-01 16:05:55 +0800

    hw3 upgrade

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -37,7 +37,8 @@ def analyse():
             html = ret.text
 
             dict["phone"] = re.findall(r"(?<=[^\w])(\d{7,8}|\d{11})(?=[^\w])", html)
-            dict["name"] = re.findall(r"[(姓名)(name)][:：](.*?)[,.;\"，。；“”]", html)
+            dict["name"] = re.findall(r"(?<=name[:：])(.*?)[,.;\"，。；“”]", html)
+            dict["name"] = dict["name"] + re.findall(r"(?<=姓名[:：])(.*?)[,.;\"，。；“”]", html)
             dict["url"] = re.findall(r"\s*(?<=[\s,.;，。；])网址[:：]\s*(https?://.*?)(?=[,;\"，。；“”\s])", html)
             dict["shared"] = re.findall(r"[^\n\r]*链接[^\n\r]*提取码[^\n\r]*", html)
 
